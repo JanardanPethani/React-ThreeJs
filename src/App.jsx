8bd8b046e1e80cdc78812e9d8ddeef62bd8733b3
JanardanPethani <jpethani11@gmail.com>
2023-04-11 21:00:45 +0530

    Update design

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
-import "./App.css"
-import { Suspense, useEffect } from "react"
-import * as THREE from "three"
-import { Canvas, useThree } from "@react-three/fiber"
-import { Environment } from "@react-three/drei"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import "./App.css";
+import { Suspense, useEffect } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // import Sphere from "./components/Sphere";
-import Model from "./components/Model"
+import Model from "./components/Model";
 
 const CameraController = () => {
-  const { camera, gl } = useThree()
+  const { camera, gl } = useThree();
   useEffect(() => {
-    const controls = new OrbitControls(camera, gl.domElement)
-    controls.minDistance = 3
-    controls.maxDistance = 20
+    const controls = new OrbitControls(camera, gl.domElement);
+    controls.minDistance = 3;
+    controls.maxDistance = 20;
     return () => {
-      controls.dispose()
-    }
-  }, [camera, gl])
-  return null
-}
+      controls.dispose();
+    };
+  }, [camera, gl]);
+  return null;
+};
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="app">
       <div id="canvas-container">
         <Canvas shadows>
           <Suspense fallback={null}>
             {/* <Sphere /> */}
             <Model path="/demoModel.gltf" />
             <CameraController />
-            <Environment preset="sunset" background />
+            <ambientLight color={"lightpink"} intensity={1.1} />
+            <pointLight position={[100, 30, -100]} intensity={0.1} />
+            <Environment preset="night" background />
           </Suspense>
         </Canvas>
       </div>
     </div>
-  )
+  );
 }
